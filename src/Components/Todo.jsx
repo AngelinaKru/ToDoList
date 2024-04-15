cec927ecd8725654f33f8103a37506b3ab8b8574
AngelinaKru <120001730+AngelinaKru@users.noreply.github.com>
2024-04-15 11:00:01 +0300

    Logic ja JSX and CSS
    LocalStorage, inputRef, useEffect, useState + CSS

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,18 +1,49 @@
-import './CSS/Todo.css'
+import './CSS/Todo.css';
+import { useState, useRef, useEffect } from 'react';
+import TodoItems from './TodoItems';
 
 const Todo = () => {
-  return (
-      <div className="todo">
-        <div className="todo-header">To-Do list</div>
-        <div className="todo-add">
-            <input type="text" placeholder="Add a new task" className='todo-input' />
-            <div className="todo-add-btn">Add</div>
-        </div>
-        <div className="todo-list">
-            
-        </div>
-    </div>
-  )
-}
+  const [todos, setTodos] = useState([]);
+  const [count, setCount] = useState(() => {
+    const storedCount = localStorage.getItem('todosCount');
+    return storedCount ? parseInt(storedCount, 10) : 0;
+  });
+  const inputRef = useRef(null);
 
-export default Todo
+  const addTodo = () => {
+    setTodos([...todos, { no: count, text: inputRef.current.value, display: "" }]);
+    setCount(prevCount => prevCount + 1);
+    inputRef.current.value = "";
+    localStorage.setItem('todosCount', count + 1);
+  };
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todos'));
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }, 100);
+  }, [todos]);
+
+  return (
+    <div className="todo">
+      <div className="todo-header">To-Do list</div>
+      <div className="todo-add">
+        <input type="text" placeholder="Add a new task" className='todo-input' ref={inputRef} />
+        <div className="todo-add-btn" onClick={addTodo}>ADD TASK</div>
+      </div>
+      <div className="todo-list">
+        {todos.map((item, index) => (
+          <TodoItems key={index} setTodos={setTodos} no={item.no} text={item.text} display={item.display} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,9 +1,35 @@
 import './CSS/TodoItems.css'
+import cross from './Assets/cross.png'
+import square from './Assets/square.png'
+import tick from './Assets/tick.png'
 
-const TodoItems = () => {
+
+const TodoItems = ({ no, text, display, setTodos }) => {
+
+const deleteTodo = (no) => {
+  let data = JSON.parse(localStorage.getItem('todos'));
+  data = data.filter((todo)=>todo.no !== no);
+  setTodos(data);
+}
+  const toggle = (no) => {
+    let data = JSON.parse(localStorage.getItem('todos'));
+    const newData = data.map(item => {
+      if (item.no === no) {
+        return { ...item, display: item.display === "" ? "line-through" : "" };
+      }
+      return item;
+    });
+    localStorage.setItem('todos', JSON.stringify(newData));
+    setTodos(newData);
+  };
+  
   return (
-    <div>
-      
+    <div className='todoitems'>
+      <div className={`todoitems-container ${display}`} onClick={()=>{toggle(no)}}>
+        {display === "" ? <img className='todoitems-icons' src={square} alt="Blank checkbox" /> : <img className='todoitems-icons' src={tick} alt="Tick icon" />}
+        <div className="todoitems-text">{text}</div>
+      </div>
+      <img className='todoitems-cross-icon' onClick={()=>{deleteTodo(no)}} src={cross} alt="Crossed checkbox" />
     </div>
   )
 }
